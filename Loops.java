1548a6fa483613e26b78ce00fbdcdd7d6d821f66
Geetika827 <59188929+Geetika827@users.noreply.github.com>
2019-12-27 14:04:36 +0530

    Add files via upload
    loops

---
+++ Loops.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int x=10;
+    while(x<5)
+    {
+    	System.out.println("Deloitte");
+    	x++;
+    }
+    System.out.println("Complete");
+	}
+
+}
