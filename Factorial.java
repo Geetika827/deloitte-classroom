0605d1da11cc3e4c915184812f54b0877605e2e0
Geetika827 <59188929+Geetika827@users.noreply.github.com>
2019-12-27 14:01:45 +0530

    Add files via upload
    Factorial

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+package loops;
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 Scanner in=new Scanner(System.in);	
+     int n,f=1;
+     System.out.println("Enter a no");
+     n=in.nextInt();
+     while(n>0)
+     {
+    	 f*=n;
+    	 n--;
+     }
+     System.out.println("Factorial="+f);
+	}
+
+}
