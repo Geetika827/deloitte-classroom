d80a0d6f2cbc8398bd06305b82346eac8aadd5d7
Geetika827 <59188929+Geetika827@users.noreply.github.com>
2020-01-03 09:17:45 +0530

    Add files via upload

--- TestInnerClass.java
+++ TestInnerClass.java
@@ -1,27 +1,14 @@
-package com.deloitte;
-class Outer
-{
-	void outermethod()
-	{
-		System.out.println("from outer");
-	}
-	static class Inner
-	{
-		void innermethod()
-		{
-			System.out.println("from inner");
-		}
-	}
+package lambda;
+interface Inter1{
+	void show();
 }
-			
+
 public class TestInnerClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    Outer outerObj=new Outer();
-    outerObj.outermethod();
-    Outer.Inner innerobj=new Outer.Inner();
-    innerobj.innermethod();
+   Inter1 i1=()->System.out.println("Hello");
+	i1.show();
 	}
 
 }
