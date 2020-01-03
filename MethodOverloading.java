ccc4ea4175854ef3a281d5c83cbeb626b1aa4846
Geetika827 <59188929+Geetika827@users.noreply.github.com>
2020-01-03 09:13:28 +0530

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,68 @@
+package method_overloading;
+
+public class MethodOverloading { 
+int add(int a, int b)
+{
+	return a+b;
+}
+float add(float a, float b)
+{
+	return a+b;
+}
+int add(int a,int b,int c)
+{
+	return a+b+c;
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MethodOverloading a1=new MethodOverloading();
+		System.out.println(a1.add(2,3));
+		System.out.println(a1.add(2.3f, 2.3f));
+		System.out.println(a1.add(2, 3,4));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
