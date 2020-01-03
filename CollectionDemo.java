5c5e331a173499b4d508141c9caa42a9277686f5
Geetika827 <59188929+Geetika827@users.noreply.github.com>
2020-01-03 09:11:10 +0530

    Add files via upload

---
+++ CollectionDemo.java
@@ -0,0 +1,169 @@
+package com.deloitte;
+import java.util.Collection;
+import java.util.Iterator;
+import java.awt.List;
+import java.util.ArrayList;
+public class CollectionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Collection items=new ArrayList();
+		       items.add("Manish");
+		      items.add(25);
+		     items.add(true);
+		      items.add(20.6);
+		       Iterator it= items.iterator();
+		       while(it.hasNext())
+		       {
+		      	 System.out.println(it.next());
+		       }
+		       for(Object item:items)
+		       {
+		     	 System.out.println(item);
+		      }
+		       items.remove(25);
+		       System.out.println("After removing");
+		       for(Object item:items)
+		     {
+		     	 System.out.println(item);
+		       }
+		       System.out.println("size is "+items.size());}
+		 
+}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+   
